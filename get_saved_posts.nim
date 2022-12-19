5b3a857959148f20b64b38829280a79140b69d8a
Saeed Baig <amateuracademic616@gmail.com>
2022-12-19 19:10:01 +1100

    Fixed no-title bug and added prettier printing
    No-title bug was because some saved posts are comments (not posts), which have no title.

--- get_saved_posts.nim
+++ get_saved_posts.nim
@@ -33,7 +33,7 @@ let token = client.postContent(
     "https://www.reddit.com/api/v1/access_token",
     multipart=newMultipartData({"grant_type":"password", "username":Reddit_username, "password":Reddit_password})
 ).parseJson()["access_token"].getStr()
-#echo fmt"token = {token}"
+#debugEcho fmt"token = {token}"
 
 # Reset headers (otherwise the multipart data screws up GET request) and set new authorisation with token
 client.headers = newHttpHeaders({
@@ -50,38 +50,37 @@ client.headers = newHttpHeaders({
 echo "Fetching saved posts now..."
 var reddit_post_count = 0
 var after = ""
-while true: # poc - replace with while loop
-    # Max limit per request seems to be 100 (3 for now for debugging)
+while true:
+    # Max limit per request seems to be 100
     # https://www.reddit.com/dev/api#GET_user_{username}_saved
     let response = client.getContent(fmt"https://oauth.reddit.com/user/{Reddit_username}/saved?limit=100&after={after}&count={reddit_post_count}&show=all?raw_json=1")
     # TODO: Add error-handling
 
     let saved_posts = response.parseJson()["data"]["children"]
     for post_object in saved_posts:
+        let post_type = post_object["kind"].getStr()
         let post = post_object["data"]
 
+        # TODO: Refactor printing into seperate proc
         echo()
         inc(reddit_post_count)
         echo fmt"#{reddit_post_count}"
         echo post["subreddit_name_prefixed"].getStr()
+
+        # Dont convert from JSON to string (for free double quotes)
+        if post_type == "t3":   # Link (normal post) (TODO make enum)
+            echo post["title"]
+        elif post_type == "t1": # Comment
+            echo post["body"]
+
         echo fmt"https://www.reddit.com{post[""permalink""].getStr()}"
-        echo post["title"]  # dont convert from JSON->string for free double quotes
-        #if "title" in post: echo post["title"]
-        #[ 
-           TODO: Why do these saved posts not have a "title"?
-           https://www.reddit.com/r/goodanimemes/comments/z7wnn9/oddly_wholesome/
-           https://www.reddit.com/r/goodanimemes/comments/z7wnn9/comment/iy8ldwb/
-
-           2nd one is probs because it's a comment and thus has no "title".
-           But why the first? Cause it's NSFW? ...
-        ]#
-
         echo()
-        # TODO: Refactor printing into seperate proc
-    #reddit_post_count += saved_posts.len
+
     echo fmt"Total #reddit-posts so far = {reddit_post_count}"
 
     after = response.parseJson()["data"]["after"].getStr()
     if after == "":
+        echo "All done. Bye :)"
         break
-    debugEcho fmt"after = '{after}'"
+    echo "Fetching more posts..."
+    #debugEcho fmt"after = '{after}'"
