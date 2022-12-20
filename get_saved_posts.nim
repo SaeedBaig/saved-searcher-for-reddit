97a86a6911a1cc46d0c3ba9dccd74394f1a8e808
Saeed Baig <amateuracademic616@gmail.com>
2022-12-20 22:09:30 +1100

    Refactoring

--- get_saved_posts.nim
+++ get_saved_posts.nim
@@ -1,6 +1,6 @@
 #[
 Get my saved Reddit posts via Reddit's official REST API
-Compile with flag `-d:ssl` so the POST requests work
+(compile with flag `-d:ssl` so the HTTP-POST requests work)
 ]#
 
 import std/[httpclient, json]
@@ -8,14 +8,18 @@ from base64 import encode
 from strformat import fmt
 from strutils import isEmptyOrWhitespace
 
-# Helper object to encapsulate the relevant post details we want to display
+# Helper class to encapsulate the relevant post details we want to display
 type RedditPost = object
     sub: string   # subreddit
     main_text: string
     url: string
 
+# Type prefixes for different types of Reddit content (https://www.reddit.com/dev/api/#type_prefixes)
+type RedditEntity = enum
+    Comment="t1", Account="t2", Link="t3", Message="t4", Subreddit="t5", Award="t6"
+
 # Helper functions
-proc parsePostObjects(post_objects: JsonNode): seq[RedditPost]
+proc parsePostObjects(json_body: JsonNode): seq[RedditPost]
 proc printPostDetails(id_num: int, post: RedditPost)
 
 # A brief description of our app ("<app name>/<app version>"); can be anything
@@ -26,7 +30,7 @@ const APP_SECRET    = "HhjA4bNm7KbmhcKBgjEKAqgHi0et4A"    # secret
 
 
 when isMainModule:
-    # Prompt for Reddit username, password and target subreddit (stdout.write instead of echo so no newline)
+    # Prompt for Reddit username, password and target subreddit (stdout.write instead of echo for no newline)
     stdout.write "Enter your Reddit username: "
     let Reddit_username = readLine(stdin)
     stdout.write "Enter your Reddit password: "
@@ -44,12 +48,11 @@ when isMainModule:
     })
 
     # Send our request for an OAuth token (valid for ~2 hours)
-    # TODO: Add error-handling for when username/password incorrect
     let token = client.postContent(
         "https://www.reddit.com/api/v1/access_token",
         multipart=newMultipartData({"grant_type":"password", "username":Reddit_username, "password":Reddit_password})
     ).parseJson()["access_token"].getStr()
-    #debugEcho fmt"token = {token}"
+    # TODO: Add error-handling for when username/password incorrect (or just no token)
 
     # Reset headers (otherwise the multipart data screws up GET request) and set new authorisation with token
     client.headers = newHttpHeaders({
@@ -64,17 +67,19 @@ when isMainModule:
     # Finally can get saved posts
     echo "Fetching saved posts now..."
     var num_fetched_posts = 0
-    var num_matched_posts = 0
+    var num_matched_posts = 0   # for numbering output
+    # Can only fetch a limited number of posts at a time, so keep fetching til we recieve the `after` field to stop
     var after = ""
-    # Can only fetch a limited number of posts at a time, so keep fetching til we get the `after` field to stop
     while true:
-        # Max limit per request seems to be 100
-        # https://www.reddit.com/dev/api#GET_user_{username}_saved
+        # Max limit per request seems to be 100 (https://www.reddit.com/dev/api#GET_user_{username}_saved)
+        # TODO Add explanation of all URL params
         let response = client.getContent(fmt"https://oauth.reddit.com/user/{Reddit_username}/saved?limit=100&after={after}&count={num_fetched_posts}&show=all?raw_json=1")
         # TODO: Add error-handling
 
-        # Print saved posts that match the target subreddit
-        let saved_posts = parsePostObjects(response.parseJson()["data"]["children"])
+        let json_data = response.parseJson()["data"]
+
+        # Print saved posts from the target subreddit
+        let saved_posts = parsePostObjects(json_data)
         for post in saved_posts:
             if post.sub == target_sub:
                 inc(num_matched_posts)
@@ -83,7 +88,7 @@ when isMainModule:
         echo fmt"Total reddit posts scanned so far: {num_fetched_posts}"
 
         # Update `after` for next fetch request
-        after = response.parseJson()["data"]["after"].getStr()
+        after = json_data["after"].getStr()
         if after.isEmptyOrWhitespace():   # All done - no more saved posts to fetch
             echo "All done. Bye :)"
             break
@@ -91,29 +96,39 @@ when isMainModule:
         #debugEcho fmt"after = '{after}'"
 
 
+## Parse response JSON to return list of RedditPost objects
+proc parsePostObjects(json_body: JsonNode): seq[RedditPost] =
+    #[ There's apparently not much official documentation about Reddit's JSON; the most I could find was this archived 
+    wiki last edited in 2016: https://github.com/reddit-archive/reddit/wiki/JSON
 
-proc parsePostObjects(post_objects: JsonNode): seq[RedditPost] =
+    The best you can do is just glean what you can from the official docs and check out your own saved-posts JSON file
+    (https://www.reddit.com/user/{username}/saved.json) to grok what the fields mean
+    (might help to paste it into a JSON-formatter first, like https://jsonformatter.curiousconcept.com/) ]#
+    
     var ret: seq[RedditPost]
 
+    let post_objects = json_body["children"]
     for post_object in post_objects:
-        #[ There's apparently not much official documentation on Reddit's JSON; the best there is is this
-        archived wiki last edited in 2016: https://github.com/reddit-archive/reddit/wiki/JSON
-        The best you can do is to glean what you can from the official docs and to 
-        paste your own saved-posts JSON file ( https://www.reddit.com/user/{username}/saved.json ) 
-        into a JSON-formatter and grok what the fields mean ]#
         let post = post_object["data"]
         let post_type = post_object["kind"].getStr()
 
+        # Parse data into RedditPost object and add it to the list
         ret.add(RedditPost(
-            sub: post["subreddit_name_prefixed"].getStr(), 
-            main_text: (if post_type == "t3": post["title"].getStr() elif post_type == "t1": post["body"].getStr() else: "ERROR: Not link or comment"),
+            sub: post["subreddit_name_prefixed"].getStr(),
+            main_text: case post_type 
+                of $Link:      # Normal saved post
+                    post["title"].getStr() 
+                of $Comment:   # Saved comment; no title
+                    post["body"].getStr() 
+                else: 
+                    "ERROR: Not link or comment",
             url: fmt"https://www.reddit.com{post[""permalink""].getStr()}"
         ))
 
     return ret
 
 
-
+## Pretty-print details of reddit-post to stdout
 proc printPostDetails(id_num: int, post: RedditPost) =
     echo()
     echo fmt"#{id_num}"
